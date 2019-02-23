0fc4b3e5e11d34ab0faa6d9ad038a50be788930f
Emily Jewik <emilyjewik@gmail.com>
2019-02-23 14:14:00 -0800

    Commit will start recording

--- SpeechTextTrial/ViewController.swift
+++ SpeechTextTrial/ViewController.swift
@@ -7,22 +7,30 @@
 //
 
 import UIKit
+import Speech
 
 struct SpeechMessage {
     let text: String
     let isIncoming: Bool
 }
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, SFSpeechRecognizerDelegate {
     
     private var tableView : UITableView!
+    private let speechRecognizer = SFSpeechRecognizer(locale: Locale.init(identifier: "en-US"))!
+    private var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
+    private var recognitionTask: SFSpeechRecognitionTask?
+    private let audioEngine = AVAudioEngine()
+    private let recordButton : UIButton = UIButton()
+    
+    
     
     //if record button tapped isIncoming = true
     //if speak button tapped isIncoming = false
     
     fileprivate let cellId = "id"
     
-    let speechMessages = [
+    var speechMessages = [
          SpeechMessage(text: "Hello there I am testing right now", isIncoming: true),
          SpeechMessage(text: "Hello there I am testing right now. fdsjhafkdsfdsfdsfkafjdsflhdsfdfjdsfkhdflkdsfhdsjflhdsajfhdsjfhdsjkfdsjfasd", isIncoming: false),
          SpeechMessage(text: "Hello there I am testing right now", isIncoming: true),
@@ -44,6 +52,18 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        //recordButton = UIButton()
+        recordButton.addTarget(self, action: #selector(recordButtonTapped(sender:)), for: .touchUpInside)
+        
         
         let settingsButton = UIBarButtonItem(title: "Settings", style: .plain, target: self, action: #selector(settingsButtonTapped(sender:)))
         
@@ -149,7 +169,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
         tableView.backgroundColor = UIColor( white: 0.98, alpha: 1 )
         
-        let recordButton : UIButton = UIButton()
+        
         
         recordButton.translatesAutoresizingMaskIntoConstraints = false
         recordButton.backgroundColor = .blue
@@ -164,6 +184,43 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         trailingButtonConstraint.isActive = true
         bottomButtonConstraint.isActive = true
         heightButtonConstraint.isActive = true
+        
+        
+        
+        
+        
+        
+        
+        tableView.reloadData()
+        recordButton.isEnabled = false
+        speechRecognizer.delegate = self
+        
+        SFSpeechRecognizer.requestAuthorization { (authStatus) in
+            
+            var isButtonEnabled = false
+            
+            switch authStatus {
+            case .authorized:
+                isButtonEnabled = true
+                
+            case .denied:
+                isButtonEnabled = false
+                print("User denied access to speech recognition")
+                
+            case .restricted:
+                isButtonEnabled = false
+                print("Speech recognition restricted on this device")
+                
+            case .notDetermined:
+                isButtonEnabled = false
+                print("Speech recognition not yet authorized")
+            }
+            
+            OperationQueue.main.addOperation() {
+                self.recordButton.isEnabled = isButtonEnabled
+            }
+        }
+        
         
         
     }
@@ -214,6 +271,99 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     @objc func settingsButtonTapped(sender: UIButton!) {
         print("Settings button tapped")
+    }
+    
+    @objc func recordButtonTapped(sender: UIButton!) {
+        print("Record button tapped")
+        if audioEngine.isRunning {
+            audioEngine.stop()
+            recognitionRequest?.endAudio()
+            recordButton.isEnabled = false
+            //speakButton.isEnabled = false
+            
+        } else {
+            //speakButton.isEnabled = false
+            let speech = SpeechMessage(text: "message", isIncoming: false)
+            self.speechMessages.append(speech)
+            startRecording(speech: speech)
+            
+            
+        }
+    }
+    
+    func startRecording(speech: SpeechMessage) {
+    
+        if recognitionTask != nil {
+            recognitionTask?.cancel()
+            recognitionTask = nil
+        }
+        
+        let audioSession = AVAudioSession.sharedInstance()
+        do {
+            try audioSession.setCategory(AVAudioSession.Category.record, mode: AVAudioSession.Mode.measurement)
+            try audioSession.setActive(true, options: .notifyOthersOnDeactivation)
+            
+        } catch {
+            print("audioSession properties weren't set because of an error.")
+        }
+        
+        recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+        
+        let inputNode = audioEngine.inputNode
+        
+        guard let recognitionRequest = recognitionRequest else {
+            fatalError("Unable to create an SFSpeechAudioBufferRecognitionRequest object")
+        }
+        
+        recognitionRequest.shouldReportPartialResults = true
+        
+        recognitionTask = speechRecognizer.recognitionTask(with: recognitionRequest, resultHandler: { (result, error) in
+            var isFinal = false
+            
+            if result != nil {
+                self.tableView.cellForRow(at: IndexPath(row: self.speechMessages.count - 1, section: 0))?.textLabel?.text = result?.bestTranscription.formattedString
+                self.tableView.reloadData()
+                
+                //speech._speech = result?.bestTranscription.formattedString
+                isFinal = (result?.isFinal)!
+            }
+            
+            if error != nil || isFinal {
+                self.audioEngine.stop()
+                inputNode.removeTap(onBus: 0)
+                
+                self.recognitionRequest = nil
+                self.recognitionTask = nil
+                
+                self.recordButton.isEnabled = true
+            
+            }
+        } )
+        
+        let recordingFormat = inputNode.outputFormat(forBus: 0)
+        inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer, when) in
+            self.recognitionRequest?.append(buffer)
+            
+        }
+        
+        audioEngine.prepare()
+        
+        do {
+            try audioEngine.start()
+            }  catch {
+                    print("audioengine could not start: error")
+                }
+                
+        
+        
+    }
+    
+    func speechRecognizer(_ speechRecognizer: SFSpeechRecognizer, availabilityDidChange available: Bool) {
+        if available {
+            recordButton.isEnabled = true
+        } else {
+            recordButton.isEnabled = false
+        }
     }
     
     
